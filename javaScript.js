7962af5e6f2313a27f7e663cda048abebbd018ff
Accellerant <accellerant@gmail.com>
2022-04-26 18:21:43 -0700

    JavaScript Complete
    I know that I didn't ocmmit as often as required, so that's one of
        the biggest issues with this current commmit. Non the less,
        here's the changes before I continue with this project.
    
    ALL functions have been put into aphabetical order, and the "main"
        program is now at the bottom.
    
    cellChangeColor()
        +New function
        +Eliminated reliance of an anon-function within gridGen.
    
    eraseCells()
        +New Function
        +Eliminate anon-function for btnErase's eventListener.
    
    gameInfo()
        +New Function
        +Removed anon-function for btnInfo's eventListener.
    
    gridGen()
        -Removed Anonymous Function.
        +Gave eventListener a function: cellChangeColor.
    
    girdInCheck()
        +Added if statement to test what's passed is a number.
        +General formatting updated: false is returned if any of
            the if statements fail.
    
    newGrid()
        +New Function
        +Take in user input and make a grid from 1-100.
    
    removeGrid()
        +New Function
        +Delete the entire grid.

--- javaScript.js
+++ javaScript.js
@@ -1,86 +1,129 @@
 /*
-Create a size x size grid for the page.
-Takes only a size of 1-100. Returns true/false.
+Change the color of the cell in their respective row.
+By default, it will change from white to black.
+*/
+function cellChangeColor() {
+    this.style.background = 'black';
+}
+
+/*
+Set each cell to white, "erasing" all cells.
+*/
+function eraseCells() {
+    const cells = document.querySelectorAll('[class*="cell"]');
+    
+    cells.forEach((cell) => {
+        cell.style.background = 'white';
+    })
+}
+
+/*
+Display how to 'play' Etch-A-Sketch.
+*/
+function gameInfo() {
+    const msgInfo = 
+        `
+        This is a \"Etch-A-Sketch\" pseudo clone!\n
+        Simply mouse overthe grid to change it from white to black.\n
+        Clicking \"Erase\" Will wipe the entire grid.\n
+        Clicking \"New Grid\" will allow you to make a new
+        grid from size 1-100.\n\n
+        Have fun!`;
+
+    alert(msgInfo);
+}
+
+/*
+Create a size x size grid for the page with
+rows and cells for each row.
+Utilizes gridInCheck() to be sure the size is
+a valid type/forat/range of 1-100.
+Returns true/false.
 */
 function gridGen(size) {
     let grid = document.querySelector(".grid");
 
-    if(gridInCheck) {
+    if(gridInCheck(size)) {
         for(let a = 1; a <= size; a++) {
             let gridRow = document.createElement('div');
             gridRow.classList.add("row" + a);
     
             for( let b = 1; b <= size; b++) {
                 let gridCell = document.createElement('div');
-                gridCell.classList.add("cell" + b);
-
-                /* Target Each individual cell for color change */
-                gridCell.addEventListener('mouseover', () => {
-                    cellChangeColor(gridCell);
-                });
-
+                gridCell.classList.add("cell" + b);         
+                gridCell.addEventListener('mouseover', cellChangeColor);
                 gridRow.appendChild(gridCell);
             }
-            
             grid.appendChild(gridRow);
-            
         }
         return true;
+        
     } else {
         return false;
     }
-    
 }
 
 /*
-Change the color of the cell in their respective row.
-By default, it will change from white to black.
-*/
-function cellChangeColor(gridCell) {
-    gridCell.style.background = 'black';
-}
-
-
-/*
-Verify that the users grid input is a valid size.
+Verify that the users grid input is a valid size and data type.
 E.G. between 1-100. Returns true/false.
 */
 function gridInCheck(size = 16) {
     const GRID_MAX = 100, GRID_MIN = 1;
 
-    if (size <= GRID_MAX && size >= GRID_MIN) {
-        return true;
+    if (typeof size === 'number') {
+        if (size <= GRID_MAX && size >= GRID_MIN) {
+            return true;
+        }
     }
-    else
-        return false;
+    return false;
 }
+
+/*
+Take in a user input and try to convert it to a number.
+Upon a valid input, which is a number, erase the grid
+and create a new one.
+*/
+function newGrid() {
+    let newGridSize, 
+        msgPrompt = "What Grid Size Would You Like?\n" +
+                    "Enter a number from 1-100.",
+        msgError = "You did not input a valid prompt!\n" +
+                    "Please enter a number from 1-100!";
+
+    do {
+        newGridSize = Number(prompt(msgPrompt));
+
+        if(!gridInCheck(newGridSize)) 
+            alert(msgError);
+        else
+            break;
+
+    } while (typeof newGridSize != NaN);
+
+    removeGrid();
+    gridGen(newGridSize);
+}
+
+/*
+Delete all currently generated rows and thus the entire grid.
+*/
+function removeGrid() {
+    const oldRow = document.querySelectorAll('[class*="row"]'); 
+   
+    oldRow.forEach((row) => {
+        row.remove();
+    });
+    
+}
+
+
 
 gridGen(16);
 
-
 const btnInfo = document.querySelector("#info");
-
-btnInfo.addEventListener('click', () => {
-    const msgInfo = 
-    `
-    This is a \"Etch-A-Sketch\" pseudo clone!\n          
-    Simply mouse overthe grid to change it from white to black.\n
-    Clicking \"Erase\" Will wipe the entire grid.\n
-    Clicking \"New Grid\" will allow you to make a new\n
-    grid from size 1-100.\n\n
-    Have fun!`;
-
-    alert(msgInfo);
-});
-
 const btnErase = document.querySelector('#erase');
+const btnNewGrid = document.querySelector('#newGrid');
 
-btnErase.addEventListener('click', () => {
-    const cells = document.querySelectorAll('[class*="cell"]');
-    
-    cells.forEach((cell) => {
-        cell.style.background = 'white';
-    })
-
-});
-
+btnInfo.addEventListener('click', gameInfo);
+btnErase.addEventListener('click', eraseCells);
+btnNewGrid.addEventListener('click', newGrid);
